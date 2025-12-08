0a78a27d2f3f88116b98edf366be2398bf7b476d
charles legault <charleslegault1992@gmail.com>
2025-12-08 17:41:06 -0500

    Ajout-etrer

--- Exercices-Collaboratifs/Exercices-Collaboratifs/menu.py
+++ Exercices-Collaboratifs/Exercices-Collaboratifs/menu.py
@@ -1,21 +1,20 @@
 # Structure de départ (Personne 1)
 def main():
     print("=== MENU DU RESTAURANT ===")
-    # Les autres ajouteront leur code ici
+
+if __name__ == "__main__":
+    main()
 
 def afficher_entrees():
-	print('''
+    print("""
 ------------- Les entrees --------------
 ~Salade César classique~
     Croûtons maison, parmesan frais, vinaigrette crémeuse.
 ~Bruschetta~
-    Tomates fraîches, basilic, huile d’olive, servie sur pain grillé.
+    Tomates fraîches, basilic, huile d'olive, servie sur pain grillé.
 ~Soupe crémeuse de légumes~
     Velouté léger, parfait pour commencer sans être trop lourd.
-''')
+""")
 
 afficher_entrees()
 
-
-if __name__ == "__main__":
-    main()
