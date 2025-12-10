6d98547d102e3caae90ee440b5792a0e08e1acf5
charles legault <charleslegault1992@gmail.com>
2025-12-10 11:56:46 -0500

    Ajout de securiter pour chiffre trop grand ou trop petit

--- pendu/affichage.py
+++ pendu/affichage.py
@@ -3,6 +3,12 @@ def affichage_pendu(erreur):
     #aucune erreur = position 0
     #possibiliter de 6 erreur total
     #Liste avec chaque dessins a des positions differentes, position 0 = 0 erreur, position 1 = 1 erreur
+    
+    if erreur < 0: # securiter pour chiffre trop petit
+        erreur = 0
+    if erreur > 6: # securiter pour chiffre trop grand
+        erreur = 6
+        
     dessin = ["""
   +---+
   |   |
